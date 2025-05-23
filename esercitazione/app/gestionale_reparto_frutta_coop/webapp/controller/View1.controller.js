216db9924b1b4b5dcf033efcd2ea1e7e3675de3d
vlad cretu <cretuvlad1999@gmail.com>
2025-05-23 12:17:02 +0000

    creo il model con i dati

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -1,57 +1,61 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+  "sap/ui/core/mvc/Controller"
 ], (Controller) => {
-    "use strict";
+  "use strict";
 
-    return Controller.extend("gestionalerepartofruttacoop.controller.View1", {
-        onInit() {
-            this.AddModel = new sap.ui.model.json.JSONModel()
+  return Controller.extend("gestionalerepartofruttacoop.controller.View1", {
+    onInit() {
+      this.AddModel = new sap.ui.model.json.JSONModel()
 
-            this.getView().setModel(this.AddModel, "AddProducts");
-            this.getView().getModel("AddProducts").setProperty("/Prodotti", []);
+      this.getView().setModel(this.AddModel, "AddProducts");
+      this.getView().getModel("AddProducts").setProperty("/Prodotti", []);
 
-            console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
+      console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
 
-            this.getRouter().getRoute("RouteView1").attachPatternMatched(this._gestioniProdottiMatched, this);
-        },
+      this.getRouter().getRoute("RouteView1").attachPatternMatched(this._gestioniProdottiMatched, this);
+    },
 
-        getRouter: function () {
+    getRouter: function () {
 
-            return sap.ui.core.UIComponent.getRouterFor(this);
-        },
+      return sap.ui.core.UIComponent.getRouterFor(this);
+    },
 
-        _gestioniProdottiMatched: function () {
-            var sUrl = this.getOwnerComponent().getModel().sServiceUrl;
-            var that = this
-            that.makeAjaxRequest(
-                sUrl + "info_studenti",
-                function (data) {
-                    that.getView().getModel("AddUser").setProperty('/Users', data.value)
+    _gestioniProdottiMatched: function () {
+      var sUrl = this.getOwnerComponent().getModel().sServiceUrl;
+      var that = this
+      that.makeAjaxRequest(
+        sUrl + "fruttarolo",
+        function (data) {
+          that.getView().getModel("AddProducts").setProperty('/Prodotti', data.value)
 
-                }.bind(that),
-                //   dove trova gli errori
-                function (error) {
-                }.bind(that));
-        },
+        }.bind(that),
+        //   dove trova gli errori
+        function (error) {}.bind(that));
+    },
 
-        makeAjaxRequest: function (url, successCallback, errorCallback) {     //funzione per le chiamate jquery.ajax
-            jQuery.ajax({
-              url: url,
-              dataType: "json",
-              success: function (data) {
-                if (typeof successCallback === "function") {
-                  successCallback(data);
-                }
-              },
-              error: function (error) {
-                if (typeof errorCallback === "function") {
-                  errorCallback(error);
-                }
-              },
-              // async: false,
-            });
+    makeAjaxRequest: function (url, successCallback, errorCallback) { //funzione per le chiamate jquery.ajax
+      jQuery.ajax({
+        url: url,
+        dataType: "json",
+        success: function (data) {
+          if (typeof successCallback === "function") {
+            successCallback(data);
           }
+        },
+        error: function (error) {
+          if (typeof errorCallback === "function") {
+            errorCallback(error);
+          }
+        },
+        // async: false,
+      });
+    },
 
 
-    });
+    esegui: function(){
+      console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
+    }
+
+
+  });
 });
