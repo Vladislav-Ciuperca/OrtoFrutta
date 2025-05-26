3c7d68b26257e3bd29d251ae0b26752b6478f293
vlad cretu <cretuvlad1999@gmail.com>
2025-05-26 13:23:36 +0000

    logica del cambiamednto per imput

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -16,10 +16,35 @@ sap.ui.define([
      
      
     },
-    // onEdit: function (evt) {
+    onEdit: function () {
 		// 	MessageToast.show(evt.getSource().getId() + " Pressed");
     //  this.byId("onSave").setVisible(false);
-		// },
+
+    this.byId("input_prodotto").setVisible(true)
+    this.byId("text_prodotto").setVisible(false)
+    
+    this.byId("input_categoria").setVisible(true)
+    this.byId("text_categoria").setVisible(false)
+
+    this.byId("input_nome").setVisible(true)
+    this.byId("text_nome").setVisible(false)
+
+    this.byId("input_quanita").setVisible(true)
+    this.byId("text_quantita").setVisible(false)
+
+    this.byId("input_prezzo").setVisible(true)
+    this.byId("text_prezzo").setVisible(false)
+
+    this.byId("input_sconto").setVisible(true)
+    this.byId("text_sconto").setVisible(false)
+
+    this.byId("input_data").setVisible(true)
+    this.byId("text_data").setVisible(false)
+
+    this.byId("input_origine").setVisible(true)
+    this.byId("text_origine").setVisible(false)
+
+		},
 
 
 
