d164a145e6cd032a5e4cfac9a90d62dead813726
vlad cretu <cretuvlad1999@gmail.com>
2025-05-26 16:52:14 +0000

    commit

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -18,7 +18,7 @@ sap.ui.define([
     },
     onEdit: function () {
 		// 	MessageToast.show(evt.getSource().getId() + " Pressed");
-    //  this.byId("onSave").setVisible(false);
+     this.byId("onSave").setVisible(false);
 
     this.byId("input_prodotto").setVisible(true)
     this.byId("text_prodotto").setVisible(false)
@@ -45,12 +45,13 @@ sap.ui.define([
     this.byId("text_origine").setVisible(false)
 
 		},
-
+    
     onSave: function () {
       // 	MessageToast.show(evt.getSource().getId() + " Pressed");
       //  this.byId("onSave").setVisible(false);
-      this.byId("onEdit").setVisible(false);
-
+      this.byId("edit").setVisible(true);
+      this.byId("undo").setVisible(false);
+      
       this.byId("input_prodotto").setVisible(false)
       this.byId("text_prodotto").setVisible(true)
       
@@ -74,6 +75,8 @@ sap.ui.define([
   
       this.byId("input_origine").setVisible(false)
       this.byId("text_origine").setVisible(true)
+
+      
   
       },
 
