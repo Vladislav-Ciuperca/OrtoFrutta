be9eff449869691dbdc7680cd8b7b9459c2e5529
vlad cretu <cretuvlad1999@gmail.com>
2025-05-26 18:43:19 +0000

    commit finale di lunedi

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -17,36 +17,38 @@ sap.ui.define([
       
     },
     onEdit: function () {
-		// 	MessageToast.show(evt.getSource().getId() + " Pressed");
-     this.byId("onSave").setVisible(false);
+      // 	MessageToast.show(evt.getSource().getId() + " Pressed");
+      this.byId("edit").setVisible(false);
+      this.byId("undo").setVisible(true);
+      
+      this.byId("input_prodotto").setVisible(true)
+      this.byId("text_prodotto").setVisible(false)
+      
+      this.byId("input_categoria").setVisible(true)
+      this.byId("text_categoria").setVisible(false)
+      
+      this.byId("input_nome").setVisible(true)
+      this.byId("text_nome").setVisible(false)
+      
+      this.byId("input_quanita").setVisible(true)
+      this.byId("text_quantita").setVisible(false)
+      
+      this.byId("input_prezzo").setVisible(true)
+      this.byId("text_prezzo").setVisible(false)
+      
+      this.byId("input_sconto").setVisible(true)
+      this.byId("text_sconto").setVisible(false)
+      
+      this.byId("input_data").setVisible(true)
+      this.byId("text_data").setVisible(false)
+      
+      this.byId("input_origine").setVisible(true)
+      this.byId("text_origine").setVisible(false)
+      
+      console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
 
-    this.byId("input_prodotto").setVisible(true)
-    this.byId("text_prodotto").setVisible(false)
     
-    this.byId("input_categoria").setVisible(true)
-    this.byId("text_categoria").setVisible(false)
-
-    this.byId("input_nome").setVisible(true)
-    this.byId("text_nome").setVisible(false)
-
-    this.byId("input_quanita").setVisible(true)
-    this.byId("text_quantita").setVisible(false)
-
-    this.byId("input_prezzo").setVisible(true)
-    this.byId("text_prezzo").setVisible(false)
-
-    this.byId("input_sconto").setVisible(true)
-    this.byId("text_sconto").setVisible(false)
-
-    this.byId("input_data").setVisible(true)
-    this.byId("text_data").setVisible(false)
-
-    this.byId("input_origine").setVisible(true)
-    this.byId("text_origine").setVisible(false)
-
-    
-
-    
+    this.byId("modifica").setVisible(false);
 
     
 
@@ -219,35 +221,36 @@ sap.ui.define([
 >>>>>>> d164a14 (commit)
     onSave: function () {
       // 	MessageToast.show(evt.getSource().getId() + " Pressed");
-      //  this.byId("onSave").setVisible(false);
-      this.byId("edit").setVisible(true);
-      this.byId("undo").setVisible(false);
+      // console.log(this.byId("edit"));
+      this.byId("modifica").setVisible(true)
+      this.byId("undo").setVisible(false)
+
       
       this.byId("input_prodotto").setVisible(false)
       this.byId("text_prodotto").setVisible(true)
       
       this.byId("input_categoria").setVisible(false)
       this.byId("text_categoria").setVisible(true)
-  
+      
       this.byId("input_nome").setVisible(false)
       this.byId("text_nome").setVisible(true)
-  
+      
       this.byId("input_quanita").setVisible(false)
       this.byId("text_quantita").setVisible(true)
-  
+      
       this.byId("input_prezzo").setVisible(false)
       this.byId("text_prezzo").setVisible(true)
-  
+      
       this.byId("input_sconto").setVisible(false)
       this.byId("text_sconto").setVisible(true)
-  
+      
       this.byId("input_data").setVisible(false)
       this.byId("text_data").setVisible(true)
-  
+      
       this.byId("input_origine").setVisible(false)
       this.byId("text_origine").setVisible(true)
-
       
+<<<<<<< HEAD
   
       },
 
@@ -316,10 +319,15 @@ sap.ui.define([
   
       this.byId("input_prodotto").setVisible(false)
       this.byId("text_prodotto").setVisible(true)
+=======
       
-      this.byId("input_categoria").setVisible(false)
-      this.byId("text_categoria").setVisible(true)
+      // this.byId("edit").setVisible(false);
+>>>>>>> e30b454 (commit finale di lunedi)
+      
+    },
+    
   
+<<<<<<< HEAD
       this.byId("input_nome").setVisible(false)
       this.byId("text_nome").setVisible(true)
   
@@ -496,6 +504,8 @@ sap.ui.define([
 >>>>>>> 9bb1f63 (Revert "cambio origine e data")
 
 
+=======
+>>>>>>> e30b454 (commit finale di lunedi)
     getRouter: function () {
 
       return sap.ui.core.UIComponent.getRouterFor(this);
@@ -508,6 +518,10 @@ sap.ui.define([
         sUrl + "fruttarolo",
         function (data) {
           that.getView().getModel("AddProducts").setProperty('/Prodotti', data.value)
+<<<<<<< HEAD
+=======
+          console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti"));
+>>>>>>> e30b454 (commit finale di lunedi)
         }.bind(that),
         function (error) { }.bind(that));
     },
@@ -530,5 +544,17 @@ sap.ui.define([
     },
 
 
+<<<<<<< HEAD
+=======
+    // edit: function () {
+    //   console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
+
+    // },
+
+
+
+
+
+>>>>>>> e30b454 (commit finale di lunedi)
   });
 });
