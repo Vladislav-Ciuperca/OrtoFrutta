f2560fb2d72bed72ae8ca9a07169d766c995edb7
Sofia Borgato <s.borgato@student.unisi.it>
2025-05-26 13:55:11 +0000

    Revert "cambio origine e data"
    This reverts commit 12d862b101d54e22eb24cbee53ec8f7fed9c9d70.

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -181,6 +181,19 @@ sap.ui.define([
     //   this.byId("edit").setVisible(false);
     // },
 
+    // onEdit: function () {
+    //   console.log();
+
+    //   this.byId("save").setVisible(false);
+    // },
+
+
+    // onSave: function () {
+    //   console.log();
+
+    //   this.byId("edit").setVisible(false);
+    // },
+
 
 
     getRouter: function () {
