5c554bff02a1fa3caeab1296f26451e89d122486
Sofia Borgato <s.borgato@student.unisi.it>
2025-05-28 12:25:57 +0000

    taso elimina funzionante

--- esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
+++ esercitazione/app/gestionale_reparto_frutta_coop/webapp/controller/View1.controller.js
@@ -4,7 +4,7 @@ sap.ui.define([
   "sap/ui/model/FilterOperator",
 ], function (Controller, Filter, FilterOperator) {
   "use strict";
-  
+
 
   return Controller.extend("gestionalerepartofruttacoop.controller.View1", {
     onInit() {
@@ -22,15 +22,15 @@ sap.ui.define([
 
     // onSearch: function (oEvent) {
     //   console.log("scrivendo.....");
-      
+
     //   var sQuery = oEvent.getSource().getValue();
     //   var aFilters = [];
 
     //   if (sQuery && sQuery.length > 0) {
     //     var filter = new Filter([
-            
+
     //         new Filter("prodotto", FilterOperator.Contains, sQuery),
-           
+
     //       ],
     //     );
 
@@ -40,7 +40,7 @@ sap.ui.define([
     //   var oList = this.byId("tabella");
     //   var oBinding = oList.getBinding("rows");
     //   oBinding.filter(aFilters, "Application");
-      
+
 
     // },
 
@@ -78,14 +78,14 @@ sap.ui.define([
 
       this.byId("input_origine").setVisible(true)
       this.byId("text_origine").setVisible(false)
-      
+
 
       console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
       this.byId("delete").setVisible(true)
 
 
       this.byId("modifica").setVisible(false)
-      
+
 
 
     },
@@ -123,7 +123,7 @@ sap.ui.define([
     },
 
     onUndo: function () {
-      
+
       this.byId("modifica").setVisible(true)
       this.byId("undo").setVisible(false)
       this.byId("barra").setVisible(false)
@@ -151,18 +151,36 @@ sap.ui.define([
       this.byId("input_origine").setVisible(false)
       this.byId("text_origine").setVisible(true)
       this.byId("delete").setVisible(false)
-      
+
     },
 
     debug: function () {
       console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti", []));
-      
+
     },
 
+    onDelete: function (oEvent) {
+      // definisco il numero dell'indice dell'oevent
+      let numero = oEvent.getSource().getParent().sId.split("row")[oEvent.getSource().getParent().sId.split("row").length - 1]
+      console.log(numero);
+      // console log dell'elemento nell'arrey che andrò a cancellare
+      console.log("sto cancellando",this.getView().getModel("AddProducts").getProperty("/Prodotti")[numero]);
+      
+      // tolto  l'ellemento dall'arrey usando "numero"
+    this.getView().getModel("AddProducts").getProperty("/Prodotti").splice(numero,1)
+// console log del modello per vedere cos'hp cancellato
+    console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti"));
+// mi "salvo" il nuovo modello temporaneo in una variabile
+      let getModello = this.getView().getModel("AddProducts").getProperty("/Prodotti")
+      // prendo il mio modello attuale e gli dico che adesso è uguale al mio modello temporaneo vhe è getModello
+      this.getView().getModel("AddProducts").setProperty("/Prodotti",getModello)
+
+    },
+    
     add: function () {
 
       this.byId("barra").setVisible(true)
-    
+
       const newProductRow = {
         categoria: "",
         prodotto: "",
@@ -172,13 +190,13 @@ sap.ui.define([
         data_aggiornamento: new Date().toISOString(),
         origine: "",
       };
-    
+
       this.getView().getModel("AddProducts").getProperty("/Prodotti").push(newProductRow)
-      
-      let prova =  this.getView().getModel("AddProducts").getProperty("/Prodotti")
+
+      let prova = this.getView().getModel("AddProducts").getProperty("/Prodotti")
       // console.log(prova);
-      
-      this.getView().getModel("AddProducts").setProperty("/Prodotti",prova)
+
+      this.getView().getModel("AddProducts").setProperty("/Prodotti", prova)
 
       console.log(this.getView().getModel("AddProducts").getProperty("/Prodotti"));
 
@@ -206,7 +224,7 @@ sap.ui.define([
         function (data) {
           that.getView().getModel("AddProducts").setProperty('/Prodotti', data.value)
         }.bind(that),
-        function (error) {}.bind(that));
+        function (error) { }.bind(that));
     },
 
     makeAjaxRequest: function (url, successCallback, errorCallback) { //funzione per le chiamate jquery.ajax
