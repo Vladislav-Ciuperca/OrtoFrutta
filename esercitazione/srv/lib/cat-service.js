91ecfe2270b1e1d9088be50000bf833b701a1e08
vlad cretu <cretuvlad1999@gmail.com>
2025-06-10 12:30:11 +0000

    Revert "commit"
    This reverts commit 0e920d5df883a49d50da13f673948e5e331a60e9.

--- esercitazione/srv/lib/cat-service.js
+++ esercitazione/srv/lib/cat-service.js
@@ -11,7 +11,7 @@ module.exports = class CatalogFruttarolo extends cds.ApplicationService {
     }
     async functionGetFlusso() {
 
-        console.log("modifica");
+        console.log("banana");
 
 
     }
